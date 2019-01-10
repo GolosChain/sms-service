9a92c1db4d23e2a939b58720e15f1caa8ac7ceab
Oleg <oleg.pav.m@gmail.com>
2019-01-10 13:01:12 +0300

    #2 Ported logic.

--- src/Main.js
+++ src/Main.js
@@ -2,15 +2,22 @@ const core = require('gls-core-service');
 const stats = core.utils.statsClient;
 const BasicMain = core.services.BasicMain;
 const env = require('./data/env');
+const twilio = require('twilio')(env.GLS_SMS_GATE_SECRET_SID, env.GLS_TWILIO_SECRET);
 const Connector = require('./services/Connector');
+const SmsGate = require('./services/SmsGate');
+const SmsSecondCheck = require('./services/SmsSecondCheck');
+const Smsc = require('./utils/Smsc');
 
 class Main extends BasicMain {
     constructor() {
         super(stats, env);
 
         const connector = new Connector();
+        const smsc = new Smsc();
+        const smsGate = new SmsGate(smsc, twilio);
+        const smsSecondCheck = new SmsSecondCheck(smsc, twilio);
 
-        this.addNested(connector);
+        this.addNested(smsGate, smsSecondCheck, connector);
     }
 }
 

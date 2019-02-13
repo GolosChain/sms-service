59279dec8b6ad6a4f585613c799f1da3731c472b
Oleg <oleg.pav.m@gmail.com>
2019-02-13 12:22:58 +0300

    Review fixes.

--- src/utils/Smsc.js
+++ src/utils/Smsc.js
@@ -2,9 +2,11 @@ const core = require('gls-core-service');
 const Logger = core.utils.Logger;
 const request = require('request-promise-native');
 const env = require('../data/env');
+const querystring = require('querystring');
 
 const SEND_POINT = 'https://smsc.ru/sys/send.php';
 const HISTORY_POINT = 'https://smsc.ru/sys/get.php';
+const DEFAULT_PARAMS = { fmt: 3, charset: 'utf-8' };
 
 class Smsc {
     async send(phone, message) {
@@ -12,16 +14,15 @@ class Smsc {
         const login = env.GLS_SMS_GATE_LOGIN;
         const pass = env.GLS_SMS_GATE_PASS;
         const sender = env.GLS_SMSC_SENDER_NAME;
-        const query = [
-            `${SEND_POINT}`,
-            `?login=${login}`,
-            `&psw=${pass}`,
-            `&phones=${phone}`,
-            `&mes=${encodedMessage}`,
-            `&sender=${sender}`,
-            `&fmt=3`,
-            `&charset=utf-8`,
-        ].join('');
+        const queryParams = querystring.stringify({
+            login,
+            psw: pass,
+            sender,
+            phones: phone,
+            mes: encodedMessage,
+            ...DEFAULT_PARAMS,
+        });
+        const query = `${SEND_POINT}?${queryParams}`;
         const resultString = await request.get(query);
         let result;
 
@@ -46,15 +47,14 @@ class Smsc {
         const login = env.GLS_SMS_GATE_LOGIN;
         const pass = env.GLS_SMS_GATE_PASS;
         const hour = env.GLS_SMS_SECOND_CHECK_HISTORY_HOURS + 1;
-        const query = [
-            `${HISTORY_POINT}`,
-            `?get_answers=1`,
-            `&login=${login}`,
-            `&psw=${pass}`,
-            `&hour=${hour}`,
-            `&fmt=3`,
-            `&charset=utf-8`,
-        ].join('');
+        const queryParams = querystring.stringify({
+            login,
+            psw: pass,
+            hour,
+            get_answers: 1,
+            ...DEFAULT_PARAMS,
+        });
+        const query = `${HISTORY_POINT}?${queryParams}`;
         const rawHistory = await request.get(query);
         const history = JSON.parse(rawHistory);
         const result = [];
